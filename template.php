ab704446c77550c8b630fb69360669b905f8631a
agorw <58491349+agorw@users.noreply.github.com>
2020-04-07 13:30:51 +0200

    template
    update

--- template.php
+++ template.php
@@ -1,3 +1,8 @@
+<?php
+
+$content = ''; 
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,6 +14,33 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="?p=accueil">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?p=categories">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?p=ajouterfilms">Ajouter un film</a>
+                    </li>
+                </ul>
+                <form action="" method="POST" class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Recherche" aria-label="Recherche">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">ok</button>
+                </form>
+            </div>
+        </nav>
+        <hr>
+    </header>
+
     <div class="container">
         <?= $content;?>  
     </div>

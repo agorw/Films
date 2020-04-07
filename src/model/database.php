159f739c5d3f9f79462b23c72339d63920f61cc5
agorw <58491349+agorw@users.noreply.github.com>
2020-04-07 11:29:40 +0200

    Update php try catch
    correction

--- src/model/database.php
+++ src/model/database.php
@@ -3,6 +3,6 @@
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=manage_movies', 'root', '', [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
     $pdo->exec('SET CHARACTER SET utf8'); 
-} catch (\Throwable $th) {
+} catch (Exception $e) {
     echo 'Erreur de connexion a la base de donnees';
 }

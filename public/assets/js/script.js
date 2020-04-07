a483a3fc500cd975fffd7489b22b8826e2fb4b57
agorw <58491349+agorw@users.noreply.github.com>
2020-04-08 00:29:31 +0200

    js ajax
     non fini

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,11 @@
 let ajax = new XMLHttpRequest();
-ajax.onload = function () {
-    if (ajax.status >= 200){
-        console.log('success');
+let title = document.getElementById('title');
+title.addEventListener("change", function() {
+    if(title.value != ""){
+        ajax.responseType = 'json';
+        ajax.open('GET', 'http://www.omdbapi.com/?apikey=a76f9717&t=' + title.value);
+        ajax.send();
+        let data = JSON.stringify(ajax.responseURL);
+        console.log(data);
     }
-}
+});
